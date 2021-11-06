07c8eabdadce0517c5c9ec6ea7ea0bb2a05705fa
DerickLFAndrade <derickspy15@gmail.com>
2021-11-06 02:37:42 -0300

    Reorganização de código

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 function iniciar() {
-    const buscarCep =  () => {
+    const buscarCep = () => {
         const form = document.querySelector('[data-form]');
         const cp = document.querySelector('[data-cep]');
         const rua = document.querySelector('[data-rua]');
         const bairro = document.querySelector('[data-bairro]');
         const cidade = document.querySelector('[data-cidade]');
         const estado = document.querySelector('[data-estado]');
-
-      
-      
-       
         let armazenarStr = '';
-        
-      
- 
+
+
+
         form.addEventListener('submit', (e) => {
             e.preventDefault();
-           
-
             const nuns = /[a-z]/;
-            const result = nuns.test(cp.value)
+            const result = nuns.test(cp.value);
+
             if (result == false) {
 
                 const url = `https://viacep.com.br/ws/${cp.value}/json/`;
 
                 fetch(url).then(endereco => {
-                    return endereco.json();
-                }).then(endereco => {
 
+                    return endereco.json();
+
+                }).then(endereco => {
 
 
                     if (endereco.logradouro) {
@@ -38,65 +34,65 @@ function iniciar() {
                         estado.innerHTML = `Estado - ${endereco.uf}`;
 
 
-                    }
-                    console.log(endereco)
-                     if(endereco.erro == true) {
+                    };
+
+
+                    if (endereco.erro == true) {
+
                         alert("Digite um CEP valido!");
                         location.reload();
 
+                    };
 
-
-                    }
-
-
-                }
-
-                );
+                });
 
             } else {
+
                 alert("Digite um CEP válido");
                 location.reload();
-            }
-           
-           
-            
+
+            };
+
         });
 
         cp.addEventListener('focus', () => {
+
             cp.placeholder = '';
+
         });
 
         cp.addEventListener('input', () => {
 
 
-            console.log(cp.value)
             if (cp.value.length == 5 && cp.value.indexOf('-') == -1) {
-                armazenarStr = cp.value;
-                //console.log(cp.value.length)
 
-                cp.value = `${cp.value}-`
+                armazenarStr = cp.value;
+                cp.value = `${cp.value}-`;
+
             } else if (cp.value.length == 6 && cp.value.indexOf('-') == 5) {
 
-
-                //   console.log(armazenarStr, cp.value.indexOf('-'))
                 cp.value = armazenarStr;
-            }
+            };
+
             if (cp.value == false) {
-                armazenarStr = ''
-                console.log(cp.value, armazenarStr, cp.value.indexOf('-'))
+
+                armazenarStr = '';
                 cp.focus();
                 rua.innerHTML = '';
                 bairro.innerHTML = '';
                 cidade.innerHTML = '';
                 estado.innerHTML = '';
+
                 cp.addEventListener('focusout', () => {
+
                     cp.placeholder = 'ex 00000-000';
-                })
-            }
-        })
-       
+
+                });
+            };
+        });
+
 
     };
     buscarCep();
-  }
-  iniciar();
+}
+iniciar();
